493d5ffbd5167b08f60e0eaffb631544064616bd
NickWeiHaoNi <159206669+NickWeiHaoNi@users.noreply.github.com>
2024-08-29 10:37:37 +1000

    val thing

--- 07.pulseWidthModulation/07.pulseWidthModulation.ino
+++ 07.pulseWidthModulation/07.pulseWidthModulation.ino
@@ -1,5 +1,5 @@
 /*
-  Author: 
+  Author: Nick N - 29/8/24
 
   Learning Intention:
   The students will learn what 'pulse width modulation' is and how to use it to write
@@ -20,13 +20,18 @@
     https://www.tinkercad.com/things/drNDsMxGMIY?sharecode=D6x4_xTTRl_zGs7B-XGqfbQhG_xgjj3TVS_c-GNTK-c
     https://github.com/TempeHS/TempeHS_Ardunio_Bootcamp/blob/main/07.pulseWidthModulation/Bootcamp-PWMOutput.png
 */
+static unsigned int ledPin = 3;
 
-
-
-void setup() {
-  
+void setup() 
+{
+  pinMode(ledPin, OUTPUT);
+    Serial.begin(9600);
+  Serial.println("debug");
 }
 
-void loop() {
-  
+void loop() 
+{
+  int val = analogRead(A3);
+  val = val / 4;
+  analogWrite(ledPin, val);
 }
